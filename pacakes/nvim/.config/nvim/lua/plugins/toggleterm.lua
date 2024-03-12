cbdd84d2bce3de8126ccbe4a46c762489032b0c2
TKG2800 <takumi1997iwamatsu@yahoo.co.jp>
2024-03-12 20:08:32 +0900

    [Update] alacritty nvim gitignore
    alacritty
    nvim
    gitignore

---
+++ pacakes/nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {[[ things you want to change go here ]]
+  },
+  keys = {
+          { "<C-t>",":ToggleTerm size=40 direction=float <CR>", mode = "n", silent = true },
+          { "<C-t>", "<C-\\><C-n>:ToggleTerm<CR>", mode = "t", silent = true }
+  }
+}
